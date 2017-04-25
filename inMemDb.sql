c669ad60d663f3c01841bf42e0b95354c7f4b8ed
Kanojah <kanoa.hi@gmail.com>
2017-04-25 09:55:24 -1000

    got it

--- inMemDb.sql
+++ inMemDb.sql
@@ -12,18 +12,18 @@ job_type VARCHAR(10) NOT NULL
 -- inserting users into the table
 INSERT INTO users (name, age, gender, job_type)
 VALUES
-('Alice', '12', 'f', 'Student'),
-('Bob', '21', 'm', 'DevLeague'),
-('Claire', '56', 'f', 'DevLeague'),
-('David', '9', 'm', 'N/A'),
-('Eric', '61', 'm', 'DevLeague'),
-('Fred', '45', 'm', 'DevLeague'),
-('George', '38', 'm', 'DevLeague'),
-('Hannah', '15', 'f', 'N/A'),
-('Ilona', '17', 'f', 'Student'),
-('Jake', '27', 'm', 'DevLeague'),
-('Kathy', '8', 'f', 'N/A'),
-('Liam', '20', 'm', 'Student');
+('Alice', 12, 'f', 'Student'),
+('Bob', 21, 'm', 'DevLeague'),
+('Claire', 56, 'f', 'DevLeague'),
+('David', 9, 'm', 'N/A'),
+('Eric', 61, 'm', 'DevLeague'),
+('Fred', 45, 'm', 'DevLeague'),
+('George', 38, 'm', 'DevLeague'),
+('Hannah', 15, 'f', 'N/A'),
+('Ilona', 17, 'f', 'Student'),
+('Jake', 27, 'm', 'DevLeague'),
+('Kathy', 8, 'f', 'N/A'),
+('Liam', 20, 'm', 'Student');
 -- getting users by id
 SELECT *
 FROM users
@@ -54,3 +54,11 @@ WHERE job_type = 'DevLeague';
 SELECT *
 FROM users
 WHERE job_type = 'N/A';
+-- getting users who can drink
+SELECT *
+FROM users
+WHERE age > 20;
+-- grouping users by employer
+SELECT *
+FROM users
+ORDER BY job_type;
